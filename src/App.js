fc16301405eef896cdda7ba181325dcea26c5f4f
Vidun E <vidun14e@gmail.com>
2021-02-16 08:18:16 +0530

    menu items

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Menu from './Menu'
-import Categories from './Categories'
-import items from './data'
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
 
 function App() {
+
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Restaurant Menu</h2>
-        <Categories/>
-        <Menu/>
-      </header>
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   );
 }
 
